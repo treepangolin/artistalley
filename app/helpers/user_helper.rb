72f506726bf36191dee3b2ffa5e2eff9b6668009
Jacob Koenen <1koenenjacob@gmail.com>
2021-02-11 00:34:10 -0500

    Add profile bio + location & edit modal for fields

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,2 +1,17 @@
 module UserHelper
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= current_user if user_signed_in?
+  end
+
+  def resource_class
+    User
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
 end
