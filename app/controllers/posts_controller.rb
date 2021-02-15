4ef1888add2f272e07a66292cbb760ca323c171e
Jacob Koenen <1koenenjacob@gmail.com>
2021-02-15 17:28:15 -0500

    Polish fonts, add optional title to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -33,7 +33,7 @@ class PostsController < ApplicationController
   private
   
   def post_params
-    params.require(:post).permit(:body, :image)
+    params.require(:post).permit(:title, :body, :image)
   end
 
   def set_post
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_15_195635) do
+ActiveRecord::Schema.define(version: 2021_02_15_213813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2021_02_15_195635) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "title"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
