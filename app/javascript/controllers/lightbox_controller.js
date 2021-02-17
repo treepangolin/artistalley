12782e9e6993fb4123c4746510eabd73f5e6436a
Jacob Koenen <1koenenjacob@gmail.com>
2021-02-16 23:54:54 -0500

    Allow lightbox to be closed with escape

--- app/javascript/controllers/lightbox_controller.js
+++ app/javascript/controllers/lightbox_controller.js
@@ -7,4 +7,10 @@ export default class extends Controller {
     this.lightboxTarget.classList.toggle('d-none')
     event.preventDefault()
   }
+
+  close(event) {
+    if (event.key == 'Escape') {
+      this.lightboxTarget.classList.add('d-none')
+    }
+  }
 }
